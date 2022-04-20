f1793b8a0cff5c1a8884e2ecdb3a6b028e1cdb03
Bogach <roman.bogach@gmail.com>
2022-04-20 11:11:39 +0300

    Updates

--- app/src/main/java/com/example/moduleeleven/MainActivity.kt
+++ app/src/main/java/com/example/moduleeleven/MainActivity.kt
@@ -14,25 +14,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        val toolbar = binding.toolbar
+
         setContentView(binding.root)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title = "Outro titulo"
+        supportActionBar?.setLogo(R.drawable.ic_logo)
+        supportActionBar?.setDisplayUseLogoEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val activity2Intent = Intent(this, MainActivity2::class.java)
         intent.putExtra("Name", "Roman")
         intent.putExtra("Age", 37)
 
-        val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result: ActivityResult ->
-            if(result.resultCode == Activity.RESULT_OK){
-                val intent = result.data
-                if(intent?.hasExtra("result") == true){
-                    Snackbar.make(binding.root,
-                        intent.getStringExtra("result")?:"",
-                        Snackbar.LENGTH_SHORT).show()
-                }
-            }
-        }
+        //val sendIntent = Intent()
+        //sendIntent.action = Intent.ACTION_SEND
+        //sendIntent.putExtra(Intent.EXTRA_TEXT, "Minha mensagem")
+        //sendIntent.type = "text/plain"
+
+        //val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            //result: ActivityResult ->
+            //if(result.resultCode == Activity.RESULT_OK){
+               // val intent = result.data
+              //  if(intent?.hasExtra("result") == true){
+                  //  Snackbar.make(binding.root,
+                       // intent.getStringExtra("result")?:"",
+                       // Snackbar.LENGTH_SHORT).show()
+               // }
+           // }
+        //}
+
+
         binding.startActivityButton.setOnClickListener {
-            activityResult.launch(activity2Intent)
+            startActivity(activity2Intent)
         }
+       // binding.startActivityButton.setOnClickListener {
+        //    startActivity(sendIntent)
+        //}
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/moduleeleven/MainActivity2.kt
+++ app/src/main/java/com/example/moduleeleven/MainActivity2.kt
@@ -1,24 +1,100 @@
 package com.example.moduleeleven
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.moduleeleven.databinding.ActivityMain2Binding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var drawer: DrawerLayout
+    lateinit var navDrawer: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+val binding = ActivityMain2Binding.inflate(layoutInflater)
 
-        if(intent?.extras?.isEmpty == false){
-            val name = intent.getStringExtra("name")
-            val age = intent.getIntExtra("age", 0)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar2)
+
+        drawer = binding.root
+        navDrawer = binding.navView
+
+        setupToolBar()
+        setupDrawer()
+
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
+
+        //if(intent?.extras?.isEmpty == false){
+            //val name = intent.getStringExtra("name")
+         //   val age = intent.getIntExtra("age", 0)
+       // }
+
+        //val result = Intent()
+        //result.putExtra("result","Navegacao com intents")
+       // setResult(Activity.RESULT_OK, result)
+        //finish()
+    }
+
+    private fun setupDrawer(){
+            navDrawer.setNavigationItemSelectedListener { menuItem ->
+                drawer.closeDrawers()
+                when(menuItem.itemId){
+                    R.id.drawer_home ->{
+                        onBackPressed()
+                        //val intent = Intent(this, MainActivity2::class.java)
+                        //startActivity(intent)
+                        true
+                    }
+                    else -> false
+                }
+
+            }
+    }
+
+    private fun setupToolBar(){
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        drawer.openDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.second_screen_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.menu_save ->{
+                Snackbar.make(this,
+                    drawer,
+                    getString(R.string.menu_save_title),
+                    Snackbar.LENGTH_SHORT
+                ).show()
+                true
+            }
+            R.id.menu_settings ->{
+                Snackbar.make(this,
+                drawer,
+                getString(R.string.menu_settings_title),
+                Snackbar.LENGTH_SHORT
+                ).show()
+
+                true
+            }
+            else -> false
         }
-
-        val result = Intent()
-        result.putExtra("result","Navegacao com intents")
-        setResult(Activity.RESULT_OK, result)
-        finish()
     }
 }
